443e26b5c81b1c4319fe87c1f9e498c4bc97ddc0
Amarok <snowlyicewolf@outlook.com>
2024-03-24 20:31:47 +0800

    Update Ingredient.

--- src/main/java/club/someoneice/togocup/tags/TagNotSameFatalException.java
+++ src/main/java/club/someoneice/togocup/tags/TagNotSameFatalException.java
@@ -1,6 +1,8 @@
 package club.someoneice.togocup.tags;
 
 public class TagNotSameFatalException extends Exception {
+    private static final long serialVersionUID = 3895706269738194202L;
+
     TagNotSameFatalException(String s) {
         super(s);
     }
